bba319b460c9b402d98c04df7b6b0f7b9fd2f848
Usama Maqsood <39711208+UsamaMalick@users.noreply.github.com>
2019-07-18 16:34:44 +0500

    I have made two file and those are finilizaed

---
+++ index.py
@@ -0,0 +1,18 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
+def parseline(line):
+  fields = line.split(',')
+  stationID = fields[0]
+  entryType = fields[2]
+  temperature = float(fields[3]) * 0.1 * (9.0 / 5.0) + 32.0
+  return (stationID, entryType, temperature)
+
+sc = SparkContext.getOrCreate()
+lines = sc.textFile('/FileStore/tables/Temperature.txt')
+parsedLines = lines.map(parseline)
+minTemps = parsedLines.filter(lambda x: "TMIN" in x[1])
+stationTemps = minTemps.map(lambda x: (x[0], x[2]))
+stationTemps.collect()
+#minTemps = stationTemps.reduceByKey(lambda x, y: min(x,y))
+# minTemps.collect()
